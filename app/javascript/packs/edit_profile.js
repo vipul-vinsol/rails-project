8020737367d8cbcff718cde137b79f50b082fdbb
vipulbhj <vipulsworkmail@gmail.com>
2020-05-19 12:42:32 +0530

    specific selector used on select2

--- app/javascript/packs/edit_profile.js
+++ app/javascript/packs/edit_profile.js
@@ -4,8 +4,7 @@ import select2 from 'select2';
 import 'select2/dist/css/select2.css';
 
 document.addEventListener("turbolinks:load", () => {
-  // FIXME_AB: custom selector
-  $('select').select2({
+  $('.select2-target').select2({
     tags: true
   });
 });
